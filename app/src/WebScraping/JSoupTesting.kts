d7924471218bc8a68ca4c07cfa8ac6d12ac4c34d
AJ Pugh <pughaj@mail.uc.edu>
2021-06-20 00:42:32 -0400

    JSoup Extracting Sample Data

--- app/src/WebScraping/JSoupTesting.kts
+++ app/src/WebScraping/JSoupTesting.kts
@@ -23,3 +23,41 @@ class Movie {
     }
 
 }
+
+fun extractMovieData(url: String): Movie? { // <1>
+    val doc: Document
+    try {
+        doc = Jsoup.connect("$wiki$url").get()  // <2>
+    }catch (e: Exception){
+        return null
+    }
+
+    val movie = Movie() // <3>
+    doc.select(".infobox tr")   // <4>
+            .forEach { ele ->   // <5>
+                when {
+                    ele.getElementsByTag("th")?.hasClass("summary") ?: false -> {   // <6>
+                        movie.title = ele.getElementsByTag("th")?.text()
+                    }
+                    else -> {
+                        val value: String? = if (ele.getElementsByTag("li").size > 1)
+                            ele.getElementsByTag("li")
+                                    .map(Element::text)
+                                    .filter(String::isNotEmpty)
+                                    .joinToString(", ") else
+                            ele.getElementsByTag("td")?.first()?.text() // <7>
+
+                        when (ele.getElementsByTag("th")?.first()?.text()) {    // <8>
+                            "Directed by" -> movie.directedBy = value ?: ""
+                            "Produced by" -> movie.producedBy = value ?: ""
+                            "Written by" -> movie.writtenBy = value ?: ""
+                            "Starring" -> movie.starring = value ?: ""
+                            "Music by" -> movie.musicBy = value ?: ""
+                            "Release date" -> movie.releaseDate = value ?: ""
+                            "title" -> movie.title = value ?: ""
+                        }
+                    }
+                }
+            }
+    return movie
+}
