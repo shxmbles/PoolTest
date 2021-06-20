00a65e47ce9a133e45fe3cf33ecb616b5054a7e3
AJ Pugh <pughaj@mail.uc.edu>
2021-06-20 00:41:03 -0400

    JSoup Class For Storing Data

--- app/src/WebScraping/JSoupTesting.kts
+++ app/src/WebScraping/JSoupTesting.kts
@@ -9,3 +9,17 @@ fun JSoupTesting() {
             .filter { it != null }
             .forEach { println(it) }
 }
+class Movie {
+    var title: String? = ""
+    var directedBy: String = ""
+    var producedBy: String = ""
+    var writtenBy: String = ""
+    var starring: String = ""
+    var musicBy: String = ""
+    var releaseDate: String = ""
+
+    override fun toString(): String {
+        return "Movie(title='$title')"
+    }
+
+}
