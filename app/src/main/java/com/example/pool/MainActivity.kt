b160bc4f4e058b2e98c4c1f647afa2e52406fe31
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2021-06-24 12:18:58 -0400

    Added the Main View Model and Main Fragment - MVVM.
    Moved the logic to Main Fragment.
    
    Added Synthetics
    
    Added a editText
    
    Changed the Input Type to Numeric Keyboard
    
    Return statement lifted out of if statement
    
    Changed variable, and id names according to coding conventions
    
    Corrected the relative layout setup according the storyboard of the project.

--- app/src/main/java/com/example/pool/MainActivity.kt
+++ app/src/main/java/com/example/pool/MainActivity.kt
@@ -4,36 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pool.dto.Chemical
 import com.example.pool.dto.Algae
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-
-    //each chemical can be created here using the chemical class
-    val exampleChemical = Chemical(name= "example", okRange = arrayOf(1F, 5F), hoursCantSwim = 0F,
-        ozPerGallon = .005F)
-    val chlorine = Chemical(name= "chl", okRange = arrayOf(1F, 5F), hoursCantSwim = 8F,
-            ozPerGallon = .005F)
-
-
-    val alkalinity = Chemical(name= "alk", okRange = arrayOf(80F, 120F), hoursCantSwim = 0F,
-            ozPerGallon = .005F)
-    val calciumHardness = Chemical(name= "cal", okRange = arrayOf(200F, 300F), hoursCantSwim = 0F,
-            ozPerGallon = .005F)
-    val pH = Chemical(name= "pH", okRange = arrayOf(7.4F, 7.6F), hoursCantSwim = 0F,
-            ozPerGallon = .005F)
-    val cyanuricAcid = Chemical(name= "cya", okRange = arrayOf(30F, 100F), hoursCantSwim = 0F,
-            ozPerGallon = .005F)
-    val totalDissolvedSolids = Chemical(name= "tds", okRange = arrayOf(0F, 1500F), hoursCantSwim = 0F,
-            ozPerGallon = .005F)
-    val phosphates = Chemical(name= "pho", okRange = arrayOf(0F, 100F), hoursCantSwim = 0F,
-            ozPerGallon = .005F)
-    val gAlgae = Algae(type= "Green", hoursCantSwim= 0F, ozPerGallon= 0F, chlBoostPerGallon= 0F)
-    val yAlgae = Algae(type= "Yellow", hoursCantSwim= 0F, ozPerGallon= 0F, chlBoostPerGallon= 0F)
-    val bAlgae = Algae(type= "Black", hoursCantSwim= 0F, ozPerGallon= 0F, chlBoostPerGallon= 0F)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
