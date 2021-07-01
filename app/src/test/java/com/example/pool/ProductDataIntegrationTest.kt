d78c1ded73d7e93488e8bfec8ef338ae43ccb396
Katie DiPaola <katewalnut@gmail.com>
2021-06-30 21:34:36 -0400

    tests for amazon api

---
+++ app/src/test/java/com/example/pool/ProductDataIntegrationTest.kt
@@ -0,0 +1,64 @@
+package com.example.pool
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.pool.dto.Product
+import com.example.pool.service.ProductService
+import com.example.pool.ui.main.MainViewModel
+import io.mockk.mockk
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.rules.TestRule
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ProductDataIntegrationTest {
+
+    @get:Rule
+    var rule: TestRule =  InstantTaskExecutorRule()
+    lateinit var mvm: MainViewModel
+
+    var productService = mockk<ProductService>()
+
+    @Test
+    fun confirmChlorine_outputsChlorine () {
+        var product: Product = Product("Chlorine", "https://link", "image_url")
+        assertEquals("Chlorine is available for purchase on Amazon at https://link", product.toString());
+    }
+
+    @Test
+    fun callForChlorine_returnsChlorine() {
+        givenAFeedOfProductDataAreAvailable()
+        whenCallForChlorine()
+        thenResultContainsEasternRedbud()
+        assertTrue("yes"=="yes")
+    }
+
+
+    private fun givenAFeedOfProductDataAreAvailable() {
+        mvm = MainViewModel()
+    }
+
+
+    private fun whenCallForChlorine() {
+        mvm.fetchProduct(myASIN="B00PZZFG0O");
+    }
+
+    private fun thenResultContainsEasternRedbud() {
+        var chlorineCalled = false;
+        mvm.product.observeForever {
+            assertNotNull(it)
+            if (it.title == "CLOROX Pool&Spa XtraBlue 3-Inch Long Lasting Chlorinating Tablets, 5-Pound Chlorine"
+                && it.link == "https://www.amazon.com/CLOROX-Pool-Spa-XtraBlue-Chlorinating/dp/B00PZZFG0O"
+                && it.main_image == "https://images-na.ssl-images-amazon.com/images/I/616baa8-DxL.jpg") { chlorineCalled = true
+            }
+        }
+        assertTrue(chlorineCalled)
+    }
+
+}
+
