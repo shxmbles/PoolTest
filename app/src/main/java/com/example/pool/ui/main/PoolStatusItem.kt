b49065c4c5de0db7a6abbe8b8fed2616f0d95f13
Howard Hall <56894894+HowardH1@users.noreply.github.com>
2021-08-05 18:14:47 -0400

    Fixed a bug where ui icons weren't showing

--- app/src/main/java/com/example/pool/ui/main/PoolStatusItem.kt
+++ app/src/main/java/com/example/pool/ui/main/PoolStatusItem.kt
@@ -4,5 +4,5 @@ import android.text.Editable
 import org.w3c.dom.Text
 import retrofit2.http.Url
 
-data class PoolStatusItem(val imageResource: Int, val Title: String, val description: String, var poolStatus: String) {
+data class PoolStatusItem(val imageResource: Int, val Title: String, val Range: String, var poolStatus: String) {
 }
