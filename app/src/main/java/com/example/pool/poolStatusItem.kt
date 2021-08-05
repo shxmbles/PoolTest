6c18e676f5c1ef17db0598de2a67ee85bbff0cca
Blake Warner <warnerianblake@gmail.com>
2021-08-05 13:07:39 -0400

    Update poolStatusItem.kt
    added import

--- app/src/main/java/com/example/pool/poolStatusItem.kt
+++ app/src/main/java/com/example/pool/poolStatusItem.kt
@@ -2,6 +2,7 @@ package com.example.pool
 
 import android.text.Editable
 import org.w3c.dom.Text
+import retrofit2.http.Url
 
 data class poolStatusItem(val imageResource: Int, val Title: String, val description: String, var poolStatus: String) {
 }
